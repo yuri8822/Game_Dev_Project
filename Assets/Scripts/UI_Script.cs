5a13f8513f7ac3da33b0dce44b61270b036aa71b
Umar.exe <61727014+yuri8822@users.noreply.github.com>
2024-11-30 18:36:27 +0500

    Almost finished with main menu and map screen (lvl select)

--- Assets/Scripts/UI_Script.cs
+++ Assets/Scripts/UI_Script.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class UI_Script : MonoBehaviour
 {
+
+    public GameObject MainMenu;
+    public GameObject Map;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MainMenu.SetActive(true);
+        Map.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void SelectLevelClicked()
+    {
+        MainMenu.SetActive(false);
+        Map.SetActive(true);
+    }
+
+    public void BackClicked()
+    {
+        MainMenu.SetActive(true);
+        Map.SetActive(false);
+    }
+
+    public void QuitToDesktopClicked()
+    {
+        Application.Quit();
     }
 }
