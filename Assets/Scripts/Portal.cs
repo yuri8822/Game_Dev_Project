fbb7be2662b722592756f0fa8471c7f4d1080004
Muhammad Azhan <azhanmuhammad123@gmail.com>
2024-12-02 02:12:00 +0500

    level1111

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,26 +1,28 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;  
 
 public class Portal : MonoBehaviour
 {
+
+    public GameObject youWinPanel;
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Check if the object colliding with the portal is the player
+
         if (other.CompareTag("Player"))
         {
-            // Hide the player when they enter the portal
+          
             other.gameObject.SetActive(false);
 
-            // Call the method to show the win message
-            WinGame();
+        
+            youWinPanel.SetActive(true);
         }
     }
 
-    private void WinGame()
+    
+    public void OnContinueButtonPressed()
     {
-        // Display the "You Win!" message in the console
-        Debug.Log("You Win!");
-
-        // Optionally, you can add additional functionality here, 
-        // such as loading a new scene or showing a UI victory screen
+        
+        SceneManager.LoadScene("Main Menu");
     }
 }
